2d2642a48c60d1d65a2f6609866fcecd6b1d8605
RaviKonjeti <ravikonjeti1234@gmail.com>
2024-08-04 09:40:46 -0500

    fixed titleAB#4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Gen Ai for ravikonjeti build stage app devops</h1>
+    <h1>My AI App</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
